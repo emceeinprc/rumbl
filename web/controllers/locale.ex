8839b3eccbed7d93b04e778f5a59a1d4486789d7
emceeinprc <matt@seekpanda.com>
2016-08-15 00:41:44 +0800

    Began adding Chinese gettext

---
+++ web/controllers/locale.ex
@@ -0,0 +1,14 @@
+defmodule Rumbl.Locale do
+  import Plug.Conn
+
+  def init(opts), do: nil
+
+  def call(conn, _opts) do
+    case conn.params["locale"] || get_session(conn, :locale) do
+      nil     -> conn
+      locale  ->
+        Gettext.put_locale(Rumbl.Gettext, locale)
+        conn |> put_session(:locale, locale)
+    end
+  end
+end
